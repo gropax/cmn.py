cf409e8920afa753d451031d39f41ec1ffbdfc4a
Maxime Laudrin <maximedelaudrin@gmail.com>
2015-10-09 11:26:04 +0200

    Initial Commit, create pinyin conversion command

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cmn',
+      version='0.0.0',
+      description='Tool for Mandarin dictionary research and pinyin conversions',
+      url='http://www.github.com/gropax/cmn.py',
+      author='gropax',
+      author_email='maximedelaudrin@gmail.com',
+      license='MIT',
+      packages=['cmn'],
+      scripts=['bin/cmn'],
+      zip_safe=False)
